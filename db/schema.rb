138acc0effcbb7ae2e6ee747df4500a4b9973a03
WillianDDaniel <williandeivitidaniel@live.com>
2025-01-13 17:49:51 -0300

    feat: added category model to the project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_13_204050) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_13_204903) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
