aed566faaee229987a8968f5add3c2a39c996743
Vinícius Fernandes <business.fernandes22@gmail.com>
2025-01-14 16:53:11 -0300

    add category name validation
    Co-authored-by: Marcos-Guimarães <102266797+marcos-grocha@users.noreply.github.com>

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'falha quando o nome está vazio' do
+      category = Category.new(name: '')
+
+      expect(category).to_not be_valid
+    end
+  end
 end
