44fb275e546c405c1002b09f0a698cc7110a21fb
Vinícius Fernandes <business.fernandes22@gmail.com>
2025-01-14 17:01:39 -0300

    add category name uniqueness validation
    Co-authored-by: Marcos-Guimarães <102266797+marcos-grocha@users.noreply.github.com>

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Category, type: :model do
 
       expect(category).to_not be_valid
     end
+
+    it 'falha quando o nome é duplicado' do
+      Category.create!(name: 'Ruby')
+      category = Category.new(name: 'Ruby')
+
+      expect(category).to_not be_valid
+    end
   end
 end
