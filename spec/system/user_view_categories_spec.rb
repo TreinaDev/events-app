516a54799b7a43a70940f84c49b3c8cc6e7e93d1
Vinícius Fernandes <business.fernandes22@gmail.com>
2025-01-14 16:42:18 -0300

    create category list
    Co-authored-by: Marcos-Guimarães <102266797+marcos-grocha@users.noreply.github.com>

---
+++ spec/system/user_view_categories_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Usuário vê as categorias' do
+    it 'com sucesso' do
+        # usuário logado
+        Category.create!(name: 'Ruby')
+        Category.create!(name: 'Rails')
+        Category.create!(name: 'Culinária')
+
+        visit root_path
+        click_on 'Categorias'
+
+        expect(page).to have_content('Lista de Categorias')
+        expect(page).to have_content 'Ruby'
+        expect(page).to have_content 'Rails'
+        expect(page).to have_content 'Culinária'
+        expect(current_path).to eq categories_path
+    end
+end
