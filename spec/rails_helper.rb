f0d7ed9babcf823931346b0fa6c7e1bea902d7fc
gctoledo <gabrielcostatoledo@outlook.com>
2025-01-15 17:49:41 -0300

    chore: add action text
    Co-authored-by: Fabio <fabiokmain@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,10 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.fixture_paths = [
+    Rails.root.join('spec/fixtures')
+  ]
+
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
