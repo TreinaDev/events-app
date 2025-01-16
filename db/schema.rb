840773a10157b222651965b98135cac5ed03ad70
gctoledo <gabrielcostatoledo@outlook.com>
2025-01-16 16:17:27 -0300

    Add event_category
    Co-authored-by: Fabio <fabiokmain@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_15_194641) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_16_183654) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -55,6 +55,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_15_194641) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "event_categories", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_event_categories_on_category_id"
+    t.index ["event_id"], name: "index_event_categories_on_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.integer "user_id", null: false
@@ -91,5 +100,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_15_194641) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "event_categories", "categories"
+  add_foreign_key "event_categories", "events"
   add_foreign_key "events", "users"
 end
---
+++ spec/factories/event_categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :event_category do
+    event { nil }
+    category { nil }
+  end
+end
