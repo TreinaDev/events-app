265dafbdc407f2e7c582ecb774b59e3c48f4a71b
gctoledo <gabrielcostatoledo@outlook.com>
2025-01-17 16:56:07 -0300

    feat: add initial style and javascript to event creation form
    Co-authored-by: Fabio <fabiokmain@gmail.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,7 +14,6 @@ class Event < ApplicationRecord
   validates :address, presence: true, if: -> { inperson? || hybrid? }
   validates :logo, content_type: { in: [ "image/png", "image/jpeg", "image/jpg" ], message: "deve ser uma imagem do tipo PNG, JPG ou JPEG" }
   validates :banner, content_type: { in: [ "image/png", "image/jpeg", "image/jpg" ], message: "deve ser uma imagem do tipo PNG, JPG ou JPEG" }
-
   validate :participants_limit_for_unverified_user
   validate :should_have_at_least_one_category
 
@@ -27,11 +26,11 @@ class Event < ApplicationRecord
   end
 
   def participants_limit_for_unverified_user
-   if user && participants_limit
-     if user.verification_status == "unverified" && participants_limit > 30
+    return unless user && participants_limit
+
+    if user.verification_status == "unverified" && participants_limit > 30
       errors.add(:participants_limit, "do evento não pode ser maior que 30 para usuários não verificados")
-     end
-   end
+    end
   end
 
   def should_have_at_least_one_category
