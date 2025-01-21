d3153f3360112ed94419f51847dcdbc27c4c1699
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-01-21 18:04:42 -0300

    test: adds unitary tests to keyword model
    Co-authored-by: Rodrigo Moreno <rmoreno.w@gmail.com>

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'falha quando o nome está vazio' do
+      keyword = Keyword.new(value: '')
+
+      expect(keyword).to_not be_valid
+    end
+
+    it 'falha quando o nome é duplicado' do
+      Keyword.create!(value: 'Backend')
+      keyword = Keyword.new(value: 'Backend')
+
+      expect(keyword).to_not be_valid
+    end
+  end
 end
