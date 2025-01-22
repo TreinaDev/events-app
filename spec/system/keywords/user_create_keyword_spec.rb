f38d8ce5822f09d461c2ff78400a6648b9f2d4a0
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2025-01-22 15:07:17 -0300

    test: add permission tests for keywords
    Co-authored-by: Rodrigo Moreno <rmoreno.w@gmail.com>

--- spec/system/keywords/user_create_keyword_spec.rb
+++ spec/system/keywords/user_create_keyword_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 describe 'Usuário tenta criar uma palavra-chave' do
+  it 'mas não está autenticado' do
+    visit new_keyword_path
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content 'Para continuar, faça login ou registre-se.'
+  end
+
+  it 'mas não tem permissão' do
+    user = create(:user)
+    login_as user
+
+    visit new_keyword_path
+
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_content 'Você não tem autorização para acessar essa página.'
+  end
+
   it 'e consegue com sucesso' do
     user = create(:user, :admin)
     login_as user
