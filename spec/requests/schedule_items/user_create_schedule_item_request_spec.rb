7fbc80726235d491a1995430145ba1d8b58d53a2
gctoledo <gabrielcostatoledo@outlook.com>
2025-01-27 13:44:20 -0300

    feat: ensure only schedule owner can create a schedule item

--- spec/requests/schedule_items/user_create_schedule_item_request_spec.rb
+++ spec/requests/schedule_items/user_create_schedule_item_request_spec.rb
@@ -20,6 +20,25 @@ describe 'Usuário cria um item de agenda' do
     expect(response).to have_http_status :unprocessable_entity
   end
 
+  it 'e falha pois não é dono da agenda' do
+    user = FactoryBot.create(:user)
+    second_user = FactoryBot.create(:user, email: 'jose@email.com', registration_number: '30094462097')
+    event = create(:event, user: user)
+    schedule = create(:schedule, event: event)
+
+    login_as second_user
+
+    post event_schedule_items_path(event, schedule),
+      params: { schedule_item: { name: 'Palestra', description: 'Palestra sobre Rails', responsible_name: 'João',
+      responsible_email: 'joão@email.com', schedule_type: 'activity',
+      start_time: (Time.now + 1.day).change(hour: 9, min: 0, sec: 0),
+      end_time: (Time.now + 1.day).change(hour: 10, min: 0, sec: 0) }
+      }
+
+    expect(response).to redirect_to root_path
+    expect(response).to have_http_status :found
+  end
+
   it 'e cria um item de agenda com sucesso' do
     user = create(:user)
     event = create(:event, user: user)
