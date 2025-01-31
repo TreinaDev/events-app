277ebcdf2982f5891f3888be822a8eca42ec1fd9
Rodrigo Moreno <rmoreno.w@gmail.com>
2025-01-31 15:55:38 -0300

    style: adjusts forms css to adhere to a standard and adds html title and favicon
    Co-authored-by: Fábio Kazuhiro <fabiokmain@gmail.com>

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
   },
   plugins: [
-    // require('@tailwindcss/forms'),
+    require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
   ]
