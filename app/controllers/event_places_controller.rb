90463517b564b9e9226a8825e85e54a4cea76bb5
WillianDDaniel <williandeivitidaniel@live.com>
2025-02-04 15:01:34 -0300

    feat: Add event manager check and update button styles in event places

--- app/controllers/event_places_controller.rb
+++ app/controllers/event_places_controller.rb
@@ -1,6 +1,7 @@
 class EventPlacesController < ApplicationController
   layout "dashboard"
   before_action :authenticate_user!
+  before_action :check_if_event_manager
 
   def index
     @event_places = current_user.event_places
