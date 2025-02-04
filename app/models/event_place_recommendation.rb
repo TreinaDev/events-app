a9308a425653439dd5bb6c654364d5a0db7f8ea1
WillianDDaniel <williandeivitidaniel@live.com>
2025-02-04 16:47:19 -0300

    feat: Add validations for EventPlaceRecommendation model and corresponding tests

--- app/models/event_place_recommendation.rb
+++ app/models/event_place_recommendation.rb
@@ -1,3 +1,6 @@
 class EventPlaceRecommendation < ApplicationRecord
   belongs_to :event_place
+
+  validates :name, :full_address, presence: true
+  validates :phone, numericality: true, length: { in: 10..11 }
 end
--- spec/models/event_place_recommendation_spec.rb
+++ spec/models/event_place_recommendation_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe EventPlaceRecommendation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'falha quando o nome está vazio' do
+      event_place_recommendation = EventPlaceRecommendation.new(name: '')
+      event_place_recommendation.valid?
+      expect(event_place_recommendation.errors[:name]).to include 'não pode ficar em branco'
+    end
+
+    it 'falha quando o endereço completo está vazio' do
+      event_place_recommendation = EventPlaceRecommendation.new(full_address: '')
+      event_place_recommendation.valid?
+      expect(event_place_recommendation.errors[:full_address]).to include 'não pode ficar em branco'
+    end
+
+    it 'falha quando o telefone é menor que 10 caracteres' do
+      event_place_recommendation = EventPlaceRecommendation.new(phone: '123')
+      event_place_recommendation.valid?
+      expect(event_place_recommendation.errors[:phone]).to include 'é muito curto (mínimo: 10 caracteres)'
+    end
+
+    it 'falha quando o telefone é maior que 11 caracteres' do
+      event_place_recommendation = EventPlaceRecommendation.new(phone: '1234567890123')
+      event_place_recommendation.valid?
+      expect(event_place_recommendation.errors[:phone]).to include 'é muito longo (máximo: 11 caracteres)'
+    end
+
+    it 'falha quando o telefone contém letras' do
+      event_place_recommendation = EventPlaceRecommendation.new(phone: '12b4567890a')
+      event_place_recommendation.valid?
+      expect(event_place_recommendation.errors[:phone]).to include 'não é um número'
+    end
+
+    it 'é valido quando todos os atributos forem preenchidos corretamente' do
+      evente_place = create(:event_place)
+      event_place_recommendation = EventPlaceRecommendation.new(name: 'Recomendação de Local',
+        full_address: 'Endereço Completo', phone: '1234567890', event_place: evente_place
+      )
+      expect(event_place_recommendation).to be_valid
+    end
+  end
 end
