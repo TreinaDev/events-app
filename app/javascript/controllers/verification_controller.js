c6221b3827421e54ee5ab10ce0964c0e4e5dbaa0
gctoledo <gabrielcostatoledo@outlook.com>
2025-02-06 18:05:37 -0300

    style: change verifications methods and tailwilnd classes from modal

--- app/javascript/controllers/verification_controller.js
+++ app/javascript/controllers/verification_controller.js
@@ -7,12 +7,14 @@ export default class extends Controller {
     const imageUrl = event.currentTarget.getAttribute("src");
     this.imageTarget.setAttribute("src", imageUrl);
     this.modalTarget.classList.remove("hidden");
+    this.modalTarget.classList.add("flex");
   }
 
   close(event) {
     // Fecha apenas se clicar fora da imagem
     if (event.target === this.modalTarget) {
       this.modalTarget.classList.add("hidden");
+      this.modalTarget.classList.remove("flex");
     }
   }
 }
