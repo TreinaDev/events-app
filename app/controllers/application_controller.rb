e4e20799e4e48f077fd6f5295bed877221344c7d
fabio-k <fabiokmain@gmail.com>
2025-02-08 22:47:09 -0300

    refactor flash messages and add new layout with sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,18 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :current_event
+
   def after_sign_in_path_for(resource)
     dashboard_path
   end
 
   protected
+
+  def current_event
+    @event ||= Event.find_by(id: params[:event_id]) || Event.find_by(id: params[:id])
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :family_name, :registration_number ])
   end
