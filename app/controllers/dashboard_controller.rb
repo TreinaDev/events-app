7e5e91d9362643ca75e062a31eccd50901fadc54
Rodrigo Moreno <rmoreno.w@gmail.com>
2025-02-09 00:26:33 -0300

    fix: Removes unused methods from controller and from view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,18 +6,6 @@ class DashboardController < ApplicationController
   def index
     @events = current_user.events
 
-    if current_user.admin?
-      users = User.event_manager.group(:verification_status).count
-      @users_count = users.values.sum
-      @verified_users_count = users["verified"] || 0
-      @events_count = Event.count
-      @published_events_count = Event.published.count
-      @last_published_events = Event.published.order(created_at: :desc).limit(3)
-      @pending_verifications = Verification.pending.order(created_at: :desc).limit(6)
-      @pending_verifications_count = Verification.pending.count
-      @reviewed_verifications = Verification.where.not(status: :pending).order(updated_at: :desc).limit(6)
-    end
-
     if current_user.event_manager?
       @events_count = current_user.events.count
       @published_events_count = current_user.events.published.count
